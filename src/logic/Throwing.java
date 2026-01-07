6979759f8011be9444a510a733c60d6e6d419ddb
saifkenani-SW <saifkhaledkenani@gmail.com>
2026-01-08 01:42:40 +0300

    Apply random 4 sticks

--- src/logic/Throwing.java
+++ src/logic/Throwing.java
@@ -1,7 +1,9 @@
 package logic;
 
+import java.util.Random;
 public class Throwing {
     private static final Throwing instance = new Throwing();
+    private Random random = new Random();
 
     private Throwing() {
     }
@@ -9,5 +11,12 @@ public class Throwing {
         return instance;
     }
 
+    public int getResult() {
+        int result = 0;
+        for (int i = 0; i < 4; i++) {
+            result += random.nextInt(2);
+        }
 
+        return result == 0 ? 5 : result;
+    }
 }
