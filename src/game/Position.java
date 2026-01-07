46b4bea3de3d9a49f9756a8a7e7f6f8840f53573
saifkenani-SW <saifkhaledkenani@gmail.com>
2026-01-07 23:34:37 +0300

    Adding nextOne method

--- src/game/Position.java
+++ src/game/Position.java
@@ -40,29 +40,20 @@ public class Position {
     }
 
 
-    public Position nextPositionRec(int step) {
-        if (step == 0) {
-            return this;
-        }
-        Position position;
+    private Position nextOne() {
         if (row == 0) {
-            if (col == 9)
-                position = new Position(row + 1, col);
-            else
-                position = new Position(row, col + 1);
-            return position.nextPositionRec(step - 1);
-        } else if (row == 1) {
-            if (col == 0)
-                position = new Position(row + 1, col);
-            else
-                position = new Position(row, col - 1);
-            return position.nextPositionRec(step - 1);
-
-        } else {
-            position = new Position(row, col + 1);
-            return position.nextPositionRec(step - 1);
+            return col == 9
+                    ? new Position(1, col)
+                    : new Position(0, col + 1);
         }
 
+        if (row == 1) {
+            return col == 0
+                    ? new Position(2, col)
+                    : new Position(1, col - 1);
+        }
+
+        return new Position(2, col + 1);
     }
 
 
