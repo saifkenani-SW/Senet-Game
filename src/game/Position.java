8c60d6673666dc68f5b7d6e6893bc105d564e468
saifkenani-SW <saifkhaledkenani@gmail.com>
2026-01-07 22:39:16 +0300

    Adding condition to  Position

--- src/game/Position.java
+++ src/game/Position.java
@@ -1,10 +1,16 @@
 package game;
 
 public class Position {
-    private final int x;
-    private final int y;
+    final int max_x = 2, max_y = 9;
+    private int x;
+    private int y;
 
     public Position(int x, int y) {
+        if (x > max_x || y > max_y) {
+            System.err.println("Invalid Position");
+            //    System.exit(1);
+            return;
+        }
         this.x = x;
         this.y = y;
     }
