30ca16a5a7b2e7a6be26342493803117e33ca188
saifkenani-SW <saifkhaledkenani@gmail.com>
2026-01-08 01:14:12 +0300

    Adding init map and switchPlayer method

--- src/game/State.java
+++ src/game/State.java
@@ -7,4 +7,23 @@ public class State {
     private final Cell[][] board = Board.getInstance().getCells();
     Map<Position, Player> players = new HashMap<>();
     Player currentPlayer;
+
+    public State() {
+        currentPlayer = Player.CPU;
+        Position position = new Position(0, -1);
+        int numberPieces = 14;
+        for (int i = 0; i < numberPieces; i++) {
+            position = position.nextPosition(1);
+            players.put(position, currentPlayer);
+            switchPlayer();
+        }
+    }
+
+    public void switchPlayer() {
+        currentPlayer = (currentPlayer == Player.CPU)
+                ? Player.HUMAN : Player.CPU;
+        /*currentPlayer = (currentPlayer == Player.PLAYER1)
+                ? Player.PLAYER2 : Player.PLAYER1;*/
+
+    }
 }
