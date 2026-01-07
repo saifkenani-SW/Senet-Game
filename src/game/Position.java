aec3db0800c4d139c926348b37f24b7e016413e0
saifkenani-SW <saifkhaledkenani@gmail.com>
2026-01-07 22:35:36 +0300

    Adding attr to  Position

--- src/game/Position.java
+++ src/game/Position.java
@@ -1,5 +1,12 @@
 package game;
 
 public class Position {
+    private final int x;
+    private final int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
 }
