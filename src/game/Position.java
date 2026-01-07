2a77e56969fb8c8b7c59e7bae85b82519e48fd4e
saifkenani-SW <saifkhaledkenani@gmail.com>
2026-01-07 23:33:32 +0300

    Adding previosPosition method

--- src/game/Position.java
+++ src/game/Position.java
@@ -1,12 +1,12 @@
 package game;
 
 public class Position {
-    private final int max_row = 2, max_col = 9;
+    private final int maxRow = 2, maxCol = 9;
     private int row;
     private int col;
 
     public Position(int row, int col) {
-        if (row > max_row || col > max_col) {
+        if (row > maxRow || col > maxCol || row < 0 || col < 0) {
             System.err.println("Invalid Position");
             //    Scolstem.erowit(1);
             return;
@@ -15,12 +15,54 @@ public class Position {
         this.col = col;
     }
 
-    public int getrow() {
+    public int getRow() {
         return row;
     }
 
-    public int getcol() {
+    public int getCol() {
         return col;
+    }
+
+    public Position previosPosition() {
+        if (row == 1) {
+            return col == 9
+                    ? new Position(row - 1, col)
+                    : new Position(row, col + 1);
+
+        } else if (row == 2) {
+            return col == 0
+                    ? new Position(row - 1, col)
+                    : new Position(row, col - 1);
+        }
+
+        return new Position(0, col - 1);
+
+    }
+
+
+    public Position nextPositionRec(int step) {
+        if (step == 0) {
+            return this;
+        }
+        Position position;
+        if (row == 0) {
+            if (col == 9)
+                position = new Position(row + 1, col);
+            else
+                position = new Position(row, col + 1);
+            return position.nextPositionRec(step - 1);
+        } else if (row == 1) {
+            if (col == 0)
+                position = new Position(row + 1, col);
+            else
+                position = new Position(row, col - 1);
+            return position.nextPositionRec(step - 1);
+
+        } else {
+            position = new Position(row, col + 1);
+            return position.nextPositionRec(step - 1);
+        }
+
     }
 
 
