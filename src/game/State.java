217a1ef6a44f4148dec547faa49e75726b6ebaf7
saifkenani-SW <saifkhaledkenani@gmail.com>
2026-01-07 22:40:37 +0300

    Adding basic attr to State

--- src/game/State.java
+++ src/game/State.java
@@ -1,4 +1,10 @@
 package game;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class State {
+    private final Cell[][] board = new Cell[3][10];
+    Map<Position, Player> players = new HashMap<>();
+    Player currentPlayer;
 }
