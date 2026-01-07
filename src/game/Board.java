f466e352e160b6bb3ad24ebf3dfc799fd6a849f6
saifkenani-SW <saifkhaledkenani@gmail.com>
2026-01-08 00:08:57 +0300

    Adding get and init method

--- src/game/Board.java
+++ src/game/Board.java
@@ -3,8 +3,26 @@ package game;
 public class Board {
 
     private static final Board instance = new Board();
+
     private final Cell[][] cells = new Cell[3][10];
 
     private Board() {
+        initializeBoard();
+    }
+
+    public static Board getInstance() {
+        return instance;
+    }
+
+    private void initializeBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 10; j++) {
+                cells[i][j] = new Cell();
+            }
+        }
+    }
+
+    public Cell[][] getCells() {
+        return cells;
     }
 }
