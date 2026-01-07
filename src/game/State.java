1f8ca5d8e01421e003d36732cf544019bf16f9a7
saifkenani-SW <saifkhaledkenani@gmail.com>
2026-01-08 00:23:15 +0300

    Adding attr for State

--- src/game/State.java
+++ src/game/State.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class State {
-    private final Cell[][] board = new Cell[3][10];
+    private final Cell[][] board = Board.getInstance().getCells();
     Map<Position, Player> players = new HashMap<>();
     Player currentPlayer;
 }
