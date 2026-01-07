86034bbbf7fb6bcc2bde37854d49780c080cbf1c
saifkenani-SW <saifkhaledkenani@gmail.com>
2026-01-08 00:06:31 +0300

    static instance and cells

--- src/game/Board.java
+++ src/game/Board.java
@@ -1,4 +1,10 @@
 package game;
 
 public class Board {
+
+    private static final Board instance = new Board();
+    private final Cell[][] cells = new Cell[3][10];
+
+    private Board() {
+    }
 }
