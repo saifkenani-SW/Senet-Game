8e460869ff7f38d3ae28f5a360dc22547beb67de
saifkenani-SW <saifkhaledkenani@gmail.com>
2026-01-08 03:13:30 +0300

    Adding Enum Type

--- src/game/Cell.java
+++ src/game/Cell.java
@@ -1,4 +1,23 @@
 package game;
 
 public class Cell {
+    Type type;
+
+    public Cell(Type type) {
+        this.type = type;
+    }
+
+    public Type getType() {
+        return type;
+    }
+}
+
+enum Type {
+    NORMAL,
+    NEW_BEGINNING,
+    INSPECTION,
+    RETURN,
+    TREE,
+    TOW,
+    FREEDOM;
 }
