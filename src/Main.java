02da6b369ec0339befbc1b69265de3081281833c
saifkenani-SW <saifkhaledkenani@gmail.com>
2026-01-08 05:45:29 +0300

    Updating main to tow player

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import game.Player;
+import game.Position;
+import game.State;
+import game.Type;
+import logic.Move;
+import logic.Play;
+
 
 public class Main {
     public static void main(String[] args) {
 
+        Play play = new Play(new State());
+        play.play();
+        //System.out.println(new State());
     }
 }
