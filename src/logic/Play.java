1b1f6d94370a4c8c027cf437b90daaf52c670fc7
saifkenani-SW <saifkhaledkenani@gmail.com>
2026-01-08 05:38:47 +0300

    Adding Player to Human vs Human play and test Move logic

---
+++ src/logic/Play.java
@@ -0,0 +1,80 @@
+package logic;
+
+import game.*;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Play {
+    private State state;
+    private Throwing throwing = Throwing.getInstance();
+    int throwingResult = throwing.getResult();
+    private Scanner scanner = new Scanner(System.in);
+    private Move move = new Move();
+
+    public Play(State state) {
+        this.state = state;
+    }
+
+    public State play() {
+        System.out.println("====== Senet Start ======");
+
+        while (true) {
+            if (state.checkWinning()) {
+                Player winner = state.getWinner();
+                System.err.println("Winner: " + winner + "!");
+                return state;
+            }
+
+            System.out.println("\n" + "=".repeat(40));
+            System.err.println("Player role : " + state.getCurrentPlayer());
+
+            int throwingResult = throwing.getResult();
+            System.err.println("throwing: " + throwingResult + getDiceDescription(throwingResult));
+
+            System.out.println("\n" + state.toString());
+
+            List<Position> positions = state.getCurrentPlayerPieces();
+
+
+            System.out.println("\n Available parts" + state.getCurrentPlayer());
+            for (int i = 0; i < positions.size(); i++) {
+                Position pos = positions.get(i);
+                int cellNumber = getCellNumber(pos);
+                System.out.printf("%d- part in cell : %d%n", i + 1, cellNumber);
+            }
+            System.out.print("\n enter the number of part");
+            int choice = scanner.nextInt();
+            if (choice < 1 || choice > positions.size()) {
+                System.out.println("Invalid choice");
+                continue;
+            }
+
+            Position selectedPosition = positions.get(choice - 1);
+
+            if (!move.canMove(state.getPlayers(), selectedPosition, throwingResult)) {
+                System.err.println("You can not move it now!");
+                System.out.println("Try again");
+                continue;
+            }
+//save player for "Try again"
+            Player currentPlayerBeforeMove = state.getCurrentPlayer();
+            state = move.move(state, selectedPosition, throwingResult);
+        }
+    }
+
+
+    private int getCellNumber(Position pos) {
+        if (pos.getRow() == 0) return pos.getCol() + 1;
+        if (pos.getRow() == 1) return 20 - pos.getCol();
+        return 21 + pos.getCol();
+    }
+
+    private String getDiceDescription(int diceValue) {
+        if (diceValue == 5) {
+            return "All Is White !";
+        }
+        return " Black Number :" + diceValue;
+    }
+
+}
