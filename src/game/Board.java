f7bf0d4547d4dadb06fe22ef7b5a9c0066ea17fe
saifkenani-SW <saifkhaledkenani@gmail.com>
2026-01-08 03:14:06 +0300

    Edit init grid

--- src/game/Board.java
+++ src/game/Board.java
@@ -17,7 +17,20 @@ public class Board {
     private void initializeBoard() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 10; j++) {
-                cells[i][j] = new Cell();
+                if (i == 1 && j == 5)
+                    cells[i][j] = new Cell(Type.NEW_BEGINNING);
+                else if (i == 2 && j == 5)
+                    cells[i][j] = new Cell(Type.INSPECTION);
+                else if (i == 2 && j == 6)
+                    cells[i][j] = new Cell(Type.RETURN);
+                else if (i == 2 && j == 7)
+                    cells[i][j] = new Cell(Type.TREE);
+                else if (i == 2 && j == 8)
+                    cells[i][j] = new Cell(Type.TREE);
+                else if (i == 2 && j == 9)
+                    cells[i][j] = new Cell(Type.FREEDOM);
+                else
+                    cells[i][j] = new Cell(Type.NORMAL);
             }
         }
     }
