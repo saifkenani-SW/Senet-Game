bb6abf7ff8687aba7c86543b0948bc7798bd3035
saifkenani-SW <saifkhaledkenani@gmail.com>
2026-01-08 03:55:13 +0300

    Adding base method

--- src/logic/Move.java
+++ src/logic/Move.java
@@ -1,6 +1,30 @@
 package logic;
 
+import game.*;
 
 public class Move {
+
+    public Move() {
+    }
+
+
+    public State move(State current, Position position, int step) {
+        if (!canMove(position, step)) {
+            return null;
+        }
+        State state = new State(current);
+        Position from = new Position(position);
+        Position to = new Position(position.nextPosition(step));
+
+
+        return state;
+    }
+
+    public boolean canMove(Position position, int step) {
+
+        return true;
+    }
+
+
 
 }
