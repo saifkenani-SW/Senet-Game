08574def08150600dcfcf8dd03147976956cc3aa
saifkenani-SW <saifkhaledkenani@gmail.com>
2026-01-16 15:54:34 +0300

    Updating Player to connect player with his piece

--- src/game/Player.java
+++ src/game/Player.java
@@ -1,5 +1,15 @@
 package game;
 
 public enum Player {
-    HUMAN, CPU;
+    PLAYER1(Piece.WHITE),
+    PLAYER2(Piece.BLACK);
+    private final Piece piece;
+
+    Player(Piece piece) {
+        this.piece = piece;
+    }
+
+    public Piece getPiece() {
+        return piece;
+    }
 }
