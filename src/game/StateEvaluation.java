d768eeb7b14ad8f4e799ef3d06a7a33f7702081d
saifkenani-SW <saifkhaledkenani@gmail.com>
2026-01-16 15:58:20 +0300

    Adding attr

--- src/game/StateEvaluation.java
+++ src/game/StateEvaluation.java
@@ -1,5 +1,27 @@
 package game;
 
 public class StateEvaluation {
+    State state;
+    int evaluation;
 
+    public StateEvaluation(State state, int evaluation) {
+        this.state = state;
+        this.evaluation = evaluation;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State Board) {
+        this.state = Board;
+    }
+
+    public int getEvaluation() {
+        return evaluation;
+    }
+
+    public void setEvaluation(int eval) {
+        this.evaluation = eval;
+    }
 }
